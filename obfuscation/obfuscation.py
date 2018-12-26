91a99654c81f052685a1f26d5351e2db96dbf70e
Alkesst <alec.augal@gmail.com>
2018-12-26 17:18:08 +0100

    Finished Obfuscation section

--- obfuscation/obfuscation.py
+++ obfuscation/obfuscation.py
@@ -1,4 +1,5 @@
 import base64
+import binascii
 alpha_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 alpha_lower = "abcdefghijklmnopqrstuvwxyz"
 base64_challenge = "VWtkc2EwbEliSFprVTBJeFI6SIZaMWxUUW5OaU1qbDNVSGM5UFFvPQo="
@@ -37,10 +38,42 @@ def caesar_crack(ciphered_text: str):
         print('{} {}'.format(i, caesar_cipher(ciphered_text, i)))
 
 
+def xor_cipher(to_cipher: str, key: str) -> str:
+    cipher = b''
+    for i in range(len(to_cipher)):
+        char = to_cipher[i]
+        key_char = key[i % len(key)]
+        cipher += bytes([ord(key_char) ^ ord(char)])
+    return binascii.hexlify(cipher).decode('ascii')
+
+
+def xor_cipher_crack_one_digit_key(ciphered: str):
+    for k in '0123456789':
+        clear = ''
+        for i in range(len(ciphered)):
+            char = ciphered[i]
+            clear += chr(ord(k) ^ ord(char))
+        print(k, clear)
+
+
+def xor_cipher_crack_two_digit_key(cipher: str):
+    undo_cipher = binascii.unhexlify(cipher.encode('ascii'))
+    for k1 in '0123456789':
+        for k2 in '0123456789':
+            result = ''
+            for i in range(len(undo_cipher)):
+                char = undo_cipher[i]
+                if i % 2 != 0:
+                    result += chr(ord(k1) ^ char)
+                else:
+                    result += chr(ord(k2) ^ char)
+            print('{} {} {}'.format(k1, k2, result))
+
+
 def main():
     to_cipher = input('Enter a message to cipher: ')
     offset = input('Enter an offset (leave blank to use default): ')
-    while not offset.isalnum() and len(offset) != 0:
+    while not offset.isdigit() and len(offset) != 0:
         print('That was not a number')
         offset = input('Enter an offset (leave blank to use default): ')
     if len(offset) != 0:
@@ -48,12 +81,18 @@ def main():
         caesar = caesar_cipher(to_cipher, offset)
     else:
         caesar = caesar_cipher(to_cipher)
-    print('Obfuscated version of the message using caesar cipher: ' + caesar)
+    key = input('Enter a key (for XOR cipher): ')
+    xor = xor_cipher(to_cipher, key)
+    print(f'Obfuscated version of the message using caesar cipher: {caesar}')
     print('Obfuscated version of the message using ROT13: ' + rot13(to_cipher))
     print('Obfuscated version of the message using base64: ' + base64cipher(bytes(to_cipher, 'utf')).decode('utf-8'))
+    print('Obfuscated version of the message using {} as key in xor: {}'.format(key, xor))
     print('Cracking caesar cipher...')
     caesar_crack(caesar)
+    print('Cracking xor cipher...')
+    xor_cipher_crack_one_digit_key(to_cipher)
 
 
 if __name__ == "__main__":
-    main()
+    xor_cipher_crack_two_digit_key('70155d5c45415d5011585446424c')
+    # main()
